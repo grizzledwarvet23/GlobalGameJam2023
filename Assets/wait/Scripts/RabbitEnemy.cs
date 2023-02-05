0462b8f0d3e002ea7bb55a50627919dcb6a552cf
Zayaan <72232563+grizzledwarvet23@users.noreply.github.com>
2023-02-05 15:04:00 -0600

    touch damage

--- Assets/wait/Scripts/RabbitEnemy.cs
+++ Assets/wait/Scripts/RabbitEnemy.cs
@@ -17,6 +17,13 @@ public class RabbitEnemy : MonoBehaviour
 
     public Animator bloodAnimatorOne;
     public Animator bloodAnimatorTwo;
+
+    public Transform touchDamageCheck;
+    public float touchDamageWidth;
+    public float touchDamageHeight;
+    public LayerMask whatIsPlayer;
+    Vector2 touchDamageBotLeft;
+    Vector2 touchDamageTopRight;
     
     void Start() {
         //stop the blood animations from playing
@@ -28,6 +35,7 @@ public class RabbitEnemy : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+        checkPlayerCollision();
         //move the enemy towards the x direction it is facing
         if(movingRight && transform.position.x < leftBoundary || !movingRight && transform.position.x > rightBoundary) {
             transform.position += transform.right * speed * Time.deltaTime;    
@@ -36,6 +44,16 @@ public class RabbitEnemy : MonoBehaviour
             StartCoroutine(attackRoot());
         }
         
+    }
+
+    void checkPlayerCollision() {
+        touchDamageBotLeft.Set(touchDamageCheck.position.x - (touchDamageWidth / 2), touchDamageCheck.position.y - (touchDamageHeight / 2));
+        touchDamageTopRight.Set(touchDamageCheck.position.x + (touchDamageWidth / 2), touchDamageCheck.position.y + (touchDamageHeight / 2));
+        Collider2D hit = Physics2D.OverlapArea(touchDamageBotLeft, touchDamageTopRight, whatIsPlayer);
+        if(hit != null) {
+            Debug.Log("Player Take Damage");
+        }
+
     }
 
     IEnumerator attackRoot() {
@@ -60,6 +78,22 @@ public class RabbitEnemy : MonoBehaviour
         if (health <= 0) {
             Destroy(gameObject);
         }
+
+    }
+
+    public void OnDrawGizmos()
+    {
+        touchDamageBotLeft.Set(touchDamageCheck.position.x - (touchDamageWidth / 2), touchDamageCheck.position.y - (touchDamageHeight / 2));
+        Vector2 botLeft = new Vector2(touchDamageCheck.position.x - (touchDamageWidth / 2), touchDamageCheck.position.y - (touchDamageHeight / 2));
+        Vector2 botRight = new Vector2(touchDamageCheck.position.x + (touchDamageWidth / 2), touchDamageCheck.position.y - (touchDamageHeight / 2));
+        Vector2 topRight = new Vector2(touchDamageCheck.position.x + (touchDamageWidth / 2), touchDamageCheck.position.y + (touchDamageHeight / 2));
+        Vector2 topLeft = new Vector2(touchDamageCheck.position.x - (touchDamageWidth / 2), touchDamageCheck.position.y + (touchDamageHeight / 2));
+
+ 
+        Gizmos.DrawLine(botLeft, botRight);
+        Gizmos.DrawLine(botRight, topRight);
+        Gizmos.DrawLine(topRight, topLeft);
+        Gizmos.DrawLine(topLeft, botLeft);
 
     }
 }
