c59c9c13f3ade9a5108cb9d6c0f735b69cb22f16
Zayaan <72232563+grizzledwarvet23@users.noreply.github.com>
2023-02-05 11:24:50 -0600

    added damage sound effects

--- Assets/wait/Scripts/Bullet.cs
+++ Assets/wait/Scripts/Bullet.cs
@@ -4,6 +4,8 @@ using UnityEngine;
 
 public class Bullet : MonoBehaviour
 {
+    public AudioSource damageSound;
+    public AudioSource killSound;
     public float timeUntilDeathSeconds;
     // Start is called before the first frame update
     void Start()
@@ -13,13 +15,23 @@ public class Bullet : MonoBehaviour
 
     IEnumerator Die(float time) {
         yield return new WaitForSeconds(time);
+        GetComponent<SpriteRenderer>().enabled = false;
+        GetComponent<Collider2D>().enabled = false;
+        yield return new WaitForSeconds(0.5f); //wait for the sound to finish playing
         Destroy(gameObject);
     }
 
     void OnTriggerEnter2D(Collider2D other) {
         if (other.gameObject.tag == "Enemy") {
+            if(other.gameObject.GetComponent<RabbitEnemy>().health == 1) {
+                killSound.Play();
+            } else {
+                damageSound.Play();
+            }
             other.gameObject.GetComponent<RabbitEnemy>().takeDamage(1);
+            // damageSound.Play();
             StartCoroutine(Die(0.1f));
+            
         }
         if(other.gameObject.tag == "Ground") {
             StartCoroutine(Die(0.05f));
